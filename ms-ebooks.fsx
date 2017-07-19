48eb0e9d71b004231a4139b0023d9dca64a9088f
Andre Dublin <81dublin@gmail.com>
2017-07-18 21:05:41 -0400

    init

---
+++ ms-ebooks.fsx
@@ -0,0 +1,28 @@
+#I "packages"
+
+#r "FSharp.Data/lib/net40/FSharp.Data.dll"
+
+open System
+open System.IO
+open FSharp.Data
+
+let readLines filePath = File.ReadLines(filePath)
+
+let links = readLines "Ligman_eBooks_2017.txt"
+
+let downloadFile (link : string) =
+    async {
+        let url = link.Trim()
+        let! request = Http.AsyncRequestStream url
+        let fileName = url.Substring(url.LastIndexOf("/"))
+        let path = Environment.CurrentDirectory + "\\" + fileName + ".pdf"
+        use outputFile = new FileStream(path, FileMode.Create)
+        do! request.ResponseStream.CopyToAsync outputFile |> Async.AwaitTask
+    }
+
+let go () = 
+    links    
+    |> Seq.skip 1 
+    |> Seq.map (downloadFile)
+    |> Async.Parallel
+    |> Async.RunSynchronously
